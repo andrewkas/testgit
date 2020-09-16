f326b3d94a562515476a21b9ff8a7f19f5dc9e5f
Andrey Kashpar <c_a_s@list.ru>
2020-09-16 23:34:11 +0300

    3d commit (ветка dev1)

--- src/main/java/org/home/Main.java
+++ src/main/java/org/home/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
         System.out.println("Hello, world!");
         System.out.println("second commit");//второй коммит проверка//
+        System.out.println("dev1 branch");//ветка dev1//
     }
 }
