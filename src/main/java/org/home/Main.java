60650ec5420e95422df9f82e6e9da65f250e8856
Andrey Kashpar <c_a_s@list.ru>
2020-09-16 23:37:29 +0300

    4th commit (второй коммит в ветке dev1)

--- src/main/java/org/home/Main.java
+++ src/main/java/org/home/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello, world!");
         System.out.println("second commit");//второй коммит проверка//
         System.out.println("dev1 branch");//ветка dev1//
+        System.out.println("4th commit in branch dev1");//коммит в dev1//
     }
 }
