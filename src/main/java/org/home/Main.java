2ccf92e34338903ef8b82d1e2017de5db4f06e7e
Andrey Kashpar <c_a_s@list.ru>
2020-09-16 23:52:00 +0300

    7th commit (коммит в ветке master)

--- src/main/java/org/home/Main.java
+++ src/main/java/org/home/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
         System.out.println("Hello, world!");
         System.out.println("second commit");//второй коммит проверка//
+        System.out.println("7th commit in master branch");//коммит в ветке master
     }
 }
