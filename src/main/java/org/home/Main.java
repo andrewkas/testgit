c06d714ba1ceb741219f00aa933c0482d43322f9
Andrey Kashpar <c_a_s@list.ru>
2020-09-16 23:39:28 +0300

    5th commit (коммит в ветке dev2)

--- src/main/java/org/home/Main.java
+++ src/main/java/org/home/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("second commit");//второй коммит проверка//
         System.out.println("dev1 branch");//ветка dev1//
         System.out.println("4th commit in branch dev1");//коммит в dev1//
+        System.out.println("5th commit in branch dev2");//коммит в ветке dev2
     }
 }
